87b1ad5b6f1c527fa9f067bc267765f5f0188cb8
you43allam <youssef.allam403@gmail.com>
2024-10-01 21:33:41 +0300

    create itinerary, itinerary model, itineraryfilter

---
+++ backend/models/itinerary.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+
+const itinerarySchema = new mongoose.Schema({
+  budget: {
+    type: String,
+    required: true, 
+  },
+  date: {
+    type: Date, 
+    required: true,
+  },
+
+  language: {
+    type: String, 
+    required: true,
+  },
+  price: {
+    type: String, 
+    required: true,
+  },
+  ratings: {
+    type: String, 
+    required: true,
+  },
+  tags: {
+    type: [String], // Array of tags (e.g., "outdoor", "family-friendly")
+  },
+  activities: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Activity', 
+}],
+}, { timestamps: true }); // Adds createdAt and updatedAt timestamps
+
+// Create the model based on the schema
+const itineraryModel = mongoose.model('Itinerary', itinerarySchema);
+
+module.exports = itineraryModel;
