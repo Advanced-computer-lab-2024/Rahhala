4545b76fa2c479fca4ddc90a4d5c694fe6595720
thesant0x <mahmoud7103@gmail.com>
2024-10-04 20:34:38 +0300

    add CRUD functionality for Activity Categories and Preference Tags

---
+++ src/Models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  Name: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  Age: {
+    type: Number,
+    required: true,
+  },
+  G:{
+    type: String,
+    required:false,
+  }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
