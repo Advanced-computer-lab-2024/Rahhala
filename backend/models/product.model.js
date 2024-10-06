7b069a6a42b08d038367e02b3be415322f786a6b
Youssef Abdelshafee <tojpyoumoh3b4@gmail.com>
2024-10-06 18:51:44 +0300

    Added product model and controller

---
+++ backend/models/product.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const productSchema = mongoose.Schema(
+  {
+    picture: { type: String, required: true },
+    price: { type: Number, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: false },
+    sellerName: {
+      //show seller
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "seller",
+      required: true,
+    },
+   
+    averageRating: { type: Number, default: 0 }, // Store average rating
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema); // Exporting the model
