15097c78d96d42a51d120bb37b4343350bef96a3
Moaaz Ahmed <moaazahmedlatif@gmail.com>
2024-10-06 22:39:03 +0300

    minor changes

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,20 +1,22 @@
-// src/components/ProtectedRoute.js
+// src/components/ProtectedRoute.jsx
 import React, { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
 import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
     const { auth } = useContext(AuthContext);
 
     if (auth.loading) {
-        return <div>Loading...</div>;
+        return <div>Loading...</div>; // Show a loading indicator while the auth state is being determined
     }
 
-    if (!auth.isAuthenticated) {
-        return <Navigate to="/login" />;
+    // If the user is authenticated, render the children (protected component)
+    if (auth.isAuthenticated) {
+        return children;
     }
 
-    return children;
+    // If not authenticated, redirect to login
+    return <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
