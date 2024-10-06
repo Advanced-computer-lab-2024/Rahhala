1d1cbe9b7c652c61c60c969959659c39a488bde3
yahya <alazharyyahya@gmail.com>
2024-10-06 23:25:44 +0300

    edit

--- frontend/src/pages/TouristAccount.jsx
+++ frontend/src/pages/TouristAccount.jsx
@@ -3,8 +3,18 @@ import { AuthContext } from '../context/AuthContext';
 import axiosInstance from '../utils/axiosConfig'; // Axios instance for API calls
 import axios from 'axios';
 import { useContext } from 'react';
+import { get } from 'mongoose';
 
 const TouristAccount = () => {
+    const getTourist = async() => {
+        try {
+            const response = await axiosInstance.get('/touristAccount/${auth.user.id}');
+            console.log(response);
+        } catch (error) {
+            console.error('Error fetching tourist profile:', error);
+        }
+    }
+    getTourist();
     const { auth } = useContext(AuthContext);
     console.log(auth);
     const response = axiosInstance.get('/touristAccount/${auth.user.id}');
