214a64a6a335f46db86ffcc7587f6f99e466aeed
yahya <alazharyyahya@gmail.com>
2024-10-06 23:40:59 +0300

    edits

--- frontend/src/pages/TouristAccount.jsx
+++ frontend/src/pages/TouristAccount.jsx
@@ -11,16 +11,16 @@ const TouristAccount = () => {
     useEffect(() => {
         // Only make API call if user is authenticated
         if (auth.isAuthenticated && auth.user) {
-            const fetchItineraries = async () => {
+            const fetchTourist = async () => {
                 try {
                     const response = await axiosInstance.get(`/touristAccount/${auth.user.id}`);
-                    setItineraries(response.data);
+                    console.log("response:", response.data.profile);
                 } catch (err) {
                     setError('Failed to load itineraries.');
                 }
             };
 
-            fetchItineraries();
+            fetchTourist();
         }
     }, [auth]);
 
@@ -35,16 +35,6 @@ const TouristAccount = () => {
     return (
         <div>
             <h2>Tourist Account</h2>
-            {error && <p>{error}</p>}
-            {itineraries ? (
-                <ul>
-                    {itineraries.map((itinerary) => (
-                        <li key={itinerary._id}>{itinerary.name}</li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No itineraries found.</p>
-            )}
         </div>
     );
 };
