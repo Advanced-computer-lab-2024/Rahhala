1421f09baedf1c35c547dc8a61aeda8cb9801708
Moaaz Ahmed <moaazahmedlatif@gmail.com>
2024-10-07 16:20:12 +0300

    logout button

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -70,7 +70,7 @@ export const AuthProvider = ({ children }) => {
     };
 
     return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
+        <AuthContext.Provider value={{ auth, setAuth, logout }}>
             {children}
         </AuthContext.Provider>
     );
