202ccb878162d999602d2b95edd65394dbf1d9c7
yahya <alazharyyahya@gmail.com>
2024-10-07 16:17:09 +0300

    navBar edits

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,60 +3,70 @@ import { AuthContext } from '../context/AuthContext';
 import axiosInstance from '../utils/axiosConfig';
 import { useNavigate } from 'react-router-dom';
 import "../navbar.css";
+
 const NavBar = () => {
     const { auth } = useContext(AuthContext); // Get auth context
-    console.log(auth)
-    // auth.token=null
-    // auth.isAuthenticated=false
-    // auth.loading=true
-    if(!auth.isAuthenticated){
-        console.log("Guest")
+    let endPoints = [];
+
+    if (auth) {
+        switch (auth.user) {
+            case 'tourist':
+                endPoints = [
+                    ["My Account", "/touristAccount"],
+                    ["Itineraries", "/touristItineraries"],
+                    ["Activities", "/activities"],
+                    ["Museums", "/museums"],
+                    ["All", "/viewAll"]
+                ];
+                break;
+            case 'user':
+                console.log("Regular user");
+                break;
+            case 'guest':
+                console.log("Guest user");
+                break;
+            default:
+                endPoints = [
+                    ["Register", "/register"],
+                    ["Itineraries", "/touristItineraries"],
+                    ["Activities", "/activities"],
+                    ["Museums", "/museums"],
+                    ["All", "/viewAll"]
+                ];
+        }
     }
 
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-            Navbar
-            </a>
-            <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            >
-            <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                    Home
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    Rahhala
                 </a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">
-                    Features
-                </a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">
-                    Pricing
-                </a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                    Disabled
-                </a>
-                </li>
-            </ul>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        {endPoints.map(([name, path], index) => (
+                            <li className="nav-item" key={index}>
+                                <a className="nav-link" href={path}>
+                                    {name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
         </nav>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
