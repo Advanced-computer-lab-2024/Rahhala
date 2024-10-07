e314565161d8542c504192155f70f2a778029883
Moaaz Ahmed <moaazahmedlatif@gmail.com>
2024-10-07 23:40:33 +0300

    final

--- backend/controllers/controllerIndex.js
+++ backend/controllers/controllerIndex.js
@@ -9,7 +9,6 @@ import museumController from "./museumController.js";
 import sellerController from "./sellerController.js";
 import touristController from "./touristController.js";
 import tourGuideController from "./tourGuideController.js";
-import tagController from "./tagController.js";
 import productController from "./productController.js";
 export default {
     activityCategoryController,
@@ -23,6 +22,5 @@ export default {
     sellerController,
     touristController,
     tourGuideController,
-    tagController,
     productController
 };
