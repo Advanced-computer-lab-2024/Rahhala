49524637a16cc88b543270d622c14744970e3221
yahya <alazharyyahya@gmail.com>
2024-11-04 16:23:21 +0200

    created complaints and reviews models

---
+++ backend/models/complaint.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+// Define the Complaint schema
+const complaintSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['resolved', 'pending'],
+        default: 'pending',
+        required: true
+    }
+}, { timestamps: true }); // Adds createdAt and updatedAt timestamps
+
+// Create the Complaint model
+const Complaint = mongoose.model('Complaint', complaintSchema);
+
+// Export the Complaint model
+export default Complaint;
