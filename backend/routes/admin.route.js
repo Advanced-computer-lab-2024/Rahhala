14a6f3fa67a78909991d12ab03fa7ed7a14ea3ce
Youssef Abdelshafee <tojpyoumoh3b4@gmail.com>
2024-11-09 17:32:42 +0200

    Added necessary routes for admin

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,14 +1,32 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { 
-    addAdmin, 
-    deleteEntity,
-    changePassword,
-     } from "../controllers/admin.controller.js";
+import {
+  addAdmin,
+  deleteEntity,
+  changePassword,
+  viewPendingAdvertisers,
+  acceptAdvertiser,
+  rejectAdvertiser,
+  viewPendingTourGuides,
+  acceptTourGuide,
+  rejectTourGuide,
+  viewPendingSellers,
+  acceptSeller,
+  rejectSeller,
+} from "../controllers/admin.controller.js";
 
 const router = express.Router();
 
 router.post("/", addAdmin);
 router.delete("/:id/:entityType", deleteEntity);
 router.put("/changePassword", verifyToken, changePassword);
+router.get("/viewPendingAdvertisers", verifyToken, viewPendingAdvertisers);
+router.put("/acceptAdvertiser/:id", verifyToken, acceptAdvertiser);
+router.put("/rejectAdvertiser/:id", verifyToken, rejectAdvertiser);
+router.get("/viewPendingTourGuides", verifyToken, viewPendingTourGuides);
+router.put("/acceptTourGuide/:id", verifyToken, acceptTourGuide);
+router.put("/rejectTourGuide/:id", verifyToken, rejectTourGuide);
+router.get("/viewPendingSellers", verifyToken, viewPendingSellers);
+router.put("/acceptSeller/:id", verifyToken, acceptSeller);
+router.put("/rejectSeller/:id", verifyToken, rejectSeller);
 export default router;
