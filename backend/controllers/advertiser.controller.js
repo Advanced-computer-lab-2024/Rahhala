edc4920d78410609b27b051b2ce7dc8e9c5e2caa
Youssef Abdelshafee <tojpyoumoh3b4@gmail.com>
2024-11-09 17:23:04 +0200

    Added registers for Seller, Advertiser and Tour Guide controllers

--- backend/controllers/advertiser.controller.js
+++ backend/controllers/advertiser.controller.js
@@ -2,33 +2,33 @@ import advertiserModel from "../models/advertiser.model.js";
 
 // Add Advertiser
 export const editAdvertiser = async (req, res) => {
-    console.log("entered  editAdvertiser");
+  console.log("entered  editAdvertiser");
 
-    const { email, companyName, website, hotline, companyProfile } = req.body;
-    const userId = req.user.id;
-    try {
-        const advertiser = await advertiserModel.findById(userId);
+  const { email, companyName, website, hotline, companyProfile } = req.body;
+  const userId = req.user.id;
+  try {
+    const advertiser = await advertiserModel.findById(userId);
 
-        if (!advertiser) {
-        return res.status(404).json({ error: "Advertiser not found" });
-        }
-
-        // Update advertiser's profile details
-        advertiser.email = email || advertiser.email;
-        advertiser.companyName = companyName || advertiser.companyName;
-        advertiser.website = website || advertiser.website;
-        advertiser.hotline = hotline || advertiser.hotline;
-        advertiser.companyProfile = companyProfile || advertiser.companyProfile;
-        advertiser.profileCreated = true;
-
-        await advertiser.save();
-        res.status(200).json({
-        message: "Advertiser profile updated successfully",
-        profile: advertiser,
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Error updating advertiser profile" });
+    if (!advertiser) {
+      return res.status(404).json({ error: "Advertiser not found" });
     }
+
+    // Update advertiser's profile details
+    advertiser.email = email || advertiser.email;
+    advertiser.companyName = companyName || advertiser.companyName;
+    advertiser.website = website || advertiser.website;
+    advertiser.hotline = hotline || advertiser.hotline;
+    advertiser.companyProfile = companyProfile || advertiser.companyProfile;
+    advertiser.profileCreated = true;
+
+    await advertiser.save();
+    res.status(200).json({
+      message: "Advertiser profile updated successfully",
+      profile: advertiser,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error updating advertiser profile" });
+  }
 };
 
 // Get Advertiser by ID
@@ -51,71 +51,108 @@ export const getAdvertiserByID = async (req, res) => {
 };
 
 export const changePassword = async (req, res) => {
-    const { oldPassword, newPassword } = req.body;
-    const userID = req.user.id;
-    console.log("Change password request received with ID:", userID);
-    try {
-        // Search for the advertiser using the email
-        const advertiser = await advertiserModel.findById(userID);
-        if (!advertiser) {
-            return res.status(404).json({ message: "Advertiser not found" });
-        }
-        // Check if the old password matches
-        if (oldPassword !== advertiser.password) {
-            return res.status(400).json({ message: "Old password is incorrect" });
-        }
-
-        // Check if the new password is the same as the old password
-        if (newPassword === oldPassword) {
-            return res.status(400).json({ message: "New password cannot be the same as the old password" });
-        }
-
-        // Update the password
-        advertiser.password = newPassword;
-        await advertiser.save();
-
-        res.status(200).json({ message: "Password changed successfully" });
-    } catch (error) {
-        console.error("Error changing password:", error);
-        res.status(500).json({ message: "Error changing password" });
+  const { oldPassword, newPassword } = req.body;
+  const userID = req.user.id;
+  console.log("Change password request received with ID:", userID);
+  try {
+    // Search for the advertiser using the email
+    const advertiser = await advertiserModel.findById(userID);
+    if (!advertiser) {
+      return res.status(404).json({ message: "Advertiser not found" });
     }
-    
+    // Check if the old password matches
+    if (oldPassword !== advertiser.password) {
+      return res.status(400).json({ message: "Old password is incorrect" });
+    }
+
+    // Check if the new password is the same as the old password
+    if (newPassword === oldPassword) {
+      return res
+        .status(400)
+        .json({
+          message: "New password cannot be the same as the old password",
+        });
+    }
+
+    // Update the password
+    advertiser.password = newPassword;
+    await advertiser.save();
+
+    res.status(200).json({ message: "Password changed successfully" });
+  } catch (error) {
+    console.error("Error changing password:", error);
+    res.status(500).json({ message: "Error changing password" });
+  }
 };
 
 export const submitDocuments = async (req, res) => {
-    const { idCardImage, taxationRegistryImage } = req.body;
-    const userID = req.user.id;
+  const { idCardImage, taxationRegistryImage } = req.body;
+  const userID = req.user.id;
 
-    try {
-        const advertiser = await advertiserModel.findById(userID);
+  try {
+    const advertiser = await advertiserModel.findById(userID);
 
-        if (!advertiser) {
-            return res.status(404).json({ error: "Advertiser not found" });
-        }
-        if (!idCardImage || !taxationRegistryImage) {
-            return res.status(400).json({ error: "Both ID card image and taxation registry image are required" });
-        }
-        advertiser.idCardImage = idCardImage;
-        advertiser.taxationRegistryImage = taxationRegistryImage;
-
-        await advertiser.save();
-
-        res.status(200).json({
-            message: "Documents submitted successfully",
-            profile: advertiser,
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Error submitting documents" });
+    if (!advertiser) {
+      return res.status(404).json({ error: "Advertiser not found" });
     }
+    if (!idCardImage || !taxationRegistryImage) {
+      return res
+        .status(400)
+        .json({
+          error: "Both ID card image and taxation registry image are required",
+        });
+    }
+    advertiser.idCardImage = idCardImage;
+    advertiser.taxationRegistryImage = taxationRegistryImage;
+
+    await advertiser.save();
+
+    res.status(200).json({
+      message: "Documents submitted successfully",
+      profile: advertiser,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error submitting documents" });
+  }
 };
 // Add this function to fetch documents
 export const getDocuments = async (req, res) => {
-    try {
-        const documents = await advertiserRequestModel.find({}, 'idCardImage taxationRegistryImage');
-        res.status(200).json({ documents });
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching documents" });
-    }
+  try {
+    const documents = await advertiserRequestModel.find(
+      {},
+      "idCardImage taxationRegistryImage"
+    );
+    res.status(200).json({ documents });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching documents" });
+  }
 };
 
+//Register Advertiser
+export const registerAdvertiser = async (req, res) => {
+  console.log("entered registerAdvertiser");
 
+  const { email, password, companyName, website, hotline, companyProfile } =
+    req.body;
+
+  try {
+    const advertiser = await advertiserModel.create({
+      username,
+      idCardImage,
+      taxationRegistryImage,
+      email,
+      password,
+      websiteLink,
+      hotline,
+      companyProfile,
+      logo,
+    });
+
+    res.status(201).json({
+      message: "Advertiser registered successfully",
+      profile: advertiser,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error registering advertiser" });
+  }
+};
--- backend/controllers/seller.controller.js
+++ backend/controllers/seller.controller.js
@@ -2,7 +2,6 @@ import sellerModel from "../models/seller.model.js";
 
 // Add Seller to the Database
 export const getSeller = async (req, res) => {
-
   const id = req.user.id;
 
   try {
@@ -20,10 +19,9 @@ export const getSeller = async (req, res) => {
 
 // Edit Seller Information
 export const editSeller = async (req, res) => {
-  const  id  = req.user.id;
+  const id = req.user.id;
   console.log("entered edit seller with id ", req.user.id);
   const { name, description, email, username, logo } = req.body;
-
 
   try {
     // Find the seller by ID and update the specified fields
@@ -35,8 +33,8 @@ export const editSeller = async (req, res) => {
           description,
           email,
           username,
-          logo
-        }
+          logo,
+        },
       },
       { new: true, runValidators: true }
     );
@@ -57,96 +55,140 @@ export const editSeller = async (req, res) => {
 };
 
 export const changePassword = async (req, res) => {
-    const { oldPassword, newPassword } = req.body;
-    const userID = req.user.id;
-    console.log("Change password request received with ID:", userID);
+  const { oldPassword, newPassword } = req.body;
+  const userID = req.user.id;
+  console.log("Change password request received with ID:", userID);
 
-    try {
-        // Search for the seller using the ID
-        const seller = await sellerModel.findById(userID);
-        if (!seller) {
-            return res.status(404).json({ message: "Seller not found" });
-        }
-
-        // Check if the old password matches
-        if (oldPassword !== seller.password) {
-            return res.status(400).json({ message: "Old password is incorrect" });
-        }
-
-        // Check if the new password is the same as the old password
-        if (newPassword === oldPassword) {
-            return res.status(400).json({ message: "New password cannot be the same as the old password" });
-        }
-
-        // Update the password using findByIdAndUpdate
-        await sellerModel.findByIdAndUpdate(userID, { password: newPassword });
-
-        res.status(200).json({ message: "Password changed successfully" });
-    } catch (error) {
-        console.error("Error changing password:", error);
-        res.status(500).json({ error: "Error changing password" });
+  try {
+    // Search for the seller using the ID
+    const seller = await sellerModel.findById(userID);
+    if (!seller) {
+      return res.status(404).json({ message: "Seller not found" });
     }
+
+    // Check if the old password matches
+    if (oldPassword !== seller.password) {
+      return res.status(400).json({ message: "Old password is incorrect" });
+    }
+
+    // Check if the new password is the same as the old password
+    if (newPassword === oldPassword) {
+      return res
+        .status(400)
+        .json({
+          message: "New password cannot be the same as the old password",
+        });
+    }
+
+    // Update the password using findByIdAndUpdate
+    await sellerModel.findByIdAndUpdate(userID, { password: newPassword });
+
+    res.status(200).json({ message: "Password changed successfully" });
+  } catch (error) {
+    console.error("Error changing password:", error);
+    res.status(500).json({ error: "Error changing password" });
+  }
 };
 
 export const submitDocuments = async (req, res) => {
-    const { idCardImage, taxationRegistryImage } = req.body;
-    const userID = req.user.id;
+  const { idCardImage, taxationRegistryImage } = req.body;
+  const userID = req.user.id;
 
-    try {
-        const seller = await sellerModel.findById(userID);
+  try {
+    const seller = await sellerModel.findById(userID);
 
-        if (!seller) {
-            return res.status(404).json({ error: "Seller not found" });
-        }
-        if (!idCardImage || !taxationRegistryImage) {
-            return res.status(400).json({ error: "Both ID card image and taxation registry image are required" });
-        }
-        seller.idCardImage = idCardImage;
-        seller.taxationRegistryImage = taxationRegistryImage;
-
-        await seller.save();
-        
-        res.status(200).json({
-            message: "Documents submitted successfully",
-            profile: seller,
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Error submitting documents" });
+    if (!seller) {
+      return res.status(404).json({ error: "Seller not found" });
     }
+    if (!idCardImage || !taxationRegistryImage) {
+      return res
+        .status(400)
+        .json({
+          error: "Both ID card image and taxation registry image are required",
+        });
+    }
+    seller.idCardImage = idCardImage;
+    seller.taxationRegistryImage = taxationRegistryImage;
+
+    await seller.save();
+
+    res.status(200).json({
+      message: "Documents submitted successfully",
+      profile: seller,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error submitting documents" });
+  }
 };
 // Add this function to fetch documents
 export const getDocuments = async (req, res) => {
   try {
-      const documents = await sellerRequestModel.find({}, 'idCardImage taxationRegistryImage');
-      res.status(200).json({ documents });
+    const documents = await sellerRequestModel.find(
+      {},
+      "idCardImage taxationRegistryImage"
+    );
+    res.status(200).json({ documents });
   } catch (error) {
-      res.status(500).json({ error: "Error fetching documents" });
+    res.status(500).json({ error: "Error fetching documents" });
   }
 };
 
-
 export const acceptTerms = async (req, res) => {
-    const userID = req.user.id;
-    console.log("Accept terms and conditions request received with ID:", userID);
+  const userID = req.user.id;
+  console.log("Accept terms and conditions request received with ID:", userID);
 
-    try {
-        // Find the seller by ID and update the acceptedTermsAndConditions field
-        const updatedSeller = await sellerModel.findByIdAndUpdate(
-            userID,
-            { $set: { acceptedTermsAndConditions: true } },
-            { new: true, runValidators: true }
-        );
+  try {
+    // Find the seller by ID and update the acceptedTermsAndConditions field
+    const updatedSeller = await sellerModel.findByIdAndUpdate(
+      userID,
+      { $set: { acceptedTermsAndConditions: true } },
+      { new: true, runValidators: true }
+    );
 
-        if (!updatedSeller) {
-            return res.status(404).json({ message: "Seller not found" });
-        }
-
-        res.status(200).json({
-            message: "Terms and conditions accepted successfully",
-            profile: updatedSeller,
-        });
-    } catch (error) {
-        console.error("Error accepting terms and conditions:", error);
-        res.status(500).json({ error: "Error accepting terms and conditions" });
+    if (!updatedSeller) {
+      return res.status(404).json({ message: "Seller not found" });
     }
+
+    res.status(200).json({
+      message: "Terms and conditions accepted successfully",
+      profile: updatedSeller,
+    });
+  } catch (error) {
+    console.error("Error accepting terms and conditions:", error);
+    res.status(500).json({ error: "Error accepting terms and conditions" });
+  }
+};
+
+//Register Seller
+export const registerSeller = async (req, res) => {
+  const {
+    username,
+    email,
+    password,
+    name,
+    idCardImage,
+    taxationRegistryImage,
+    description,
+    logo,
+  } = req.body;
+
+  try {
+    const seller = await sellerModel.create({
+      username,
+      email,
+      password,
+      name,
+      idCardImage,
+      taxationRegistryImage,
+      description,
+      logo,
+    });
+
+    res
+      .status(201)
+      .json({ message: "Seller registered successfully", profile: seller });
+  } catch (error) {
+    console.error("Error registering seller:", error);
+    res.status(500).json({ error: "Error registering seller" });
+  }
 };
